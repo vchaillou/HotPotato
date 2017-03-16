c5af78aa7550408c862e831da26cfd859e9abe61
dell <chrisebongue@hotmail.fr>
2017-03-16 09:50:04 +0100

    add first stat

--- main/main.ino
+++ main/main.ino
@@ -14,6 +14,9 @@
 
 #define SEND_POTATO_TO_PLAYER_X "To:X Potato"
 
+
+
+
 const int buttonPin = D0; // Button to thow the potato to another player
 const int yellowButtonPin = D5; // Potato owner
 const int redButtonPin = D7;    // Death Indicator
@@ -36,7 +39,32 @@ int playerCount = 0;
 
 ESP8266WebServer server(WEBSERVER_PORT);
 const char * ssid = "Server_Potato";
-
+const char * ContentHtml = "<!DOCTYPE html>\
+                        <html>\
+                          <head>\
+                          <title>Potato Game</title>\
+                          </head>\
+                          <body>\
+                          </body>\
+                        </html>"; 
+const char * linkJquery = "<link rel='stylesheet' href='//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css'>\
+                           <script src='https://code.jquery.com/jquery-1.12.4.js'></script>\
+                           <script src='https://code.jquery.com/ui/1.12.1/jquery-ui.js'></script>";
+const char * contentJquery = "<script>\
+                                $( function() {\
+                                } );\
+                          </script>";
+const char * contentCss = "<style>\
+                              table{\
+                                border-style: solid;\ 
+                                border-width: 1px;\ 
+                                margin-left:40%\
+                              }\
+                              td{\
+                                border-style: solid;\ 
+                                border-width: 1px;\ 
+                              }\    
+                          </style>";
 void setupMesh() {
   mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT );
   mesh.setReceiveCallback( &receivedCallback );
@@ -125,18 +153,66 @@ void webRoot() {
   server.send(200, "text/html", getHTML());
 }
 
-String getHTML() {
-  if(player.name.length() > 0) {
-    return "Hello " + player.name;
-  }
-  return "<Form action='/set_name'><label>Name Player</label><input name='name_player'/>\
-          <button type='submit'>Save name</button></Form>";
+
+String getStatHtml(){
+  int numGame = player.nbWon + player.nbLost;
+  String str_num = String(player.num);
+  String str_lost = String(player.nbLost);
+  String str_win = String(player.nbWon);
+  String str_all = String(numGame);
+
+  String stat = "<br/>\
+                 <table>\
+                  <tr>\
+                    <td>N°</td>\
+                    <td>Name</td>\
+                    <td>Number lost</td>\
+                    <td>Number win</td>\
+                    <td>Number all Game</td>\
+                  </tr>\
+                  <tr>\
+                      <td>"+str_num+"</td>\
+                      <td>"+player.name+"</td>\
+                      <td>"+str_lost+"</td>\
+                      <td>"+str_win+"</td>\
+                      <td>"+str_all+"</td>\
+                    </tr>\
+                </table>";
+  return stat;
 }
+
+String getHTML() {
+  String tmpContentHtml = ContentHtml;
+  String tmpContentJquery = contentJquery;
+  String tmpLinkJquery = linkJquery;
+  String tmpContentCss = contentCss;
+  String tmpFormHtml = "<Form style='margin-left:40%' action='/set_name'><label>Name Player</label><input name='name_player'/>\
+                    <button type='submit'>Save name</button></Form>";
+  String replaceOcc0 = "</title>";
+  tmpContentHtml.replace(replaceOcc0, replaceOcc0 + tmpContentJquery); 
+  String replaceOcc1 = "<head>";
+  tmpContentHtml.replace(replaceOcc1, replaceOcc1 + tmpLinkJquery);
+  tmpContentHtml.replace(replaceOcc1, replaceOcc1 + tmpContentCss);
+  String replaceOcc2 =  "<body>";
+  tmpContentHtml.replace(replaceOcc2, replaceOcc2 + tmpFormHtml);
+  String statHtml = getStatHtml();
+  
+  if(player.name.length() > 0) {
+    return "Hello " + player.name + statHtml;
+  }
+  return tmpContentHtml;
+}
+
 
 void setName() {
   player.name = server.arg(0);
   Serial.println(player.name);
-  server.send(200, "text/html", getHTML());
+  if(player.nbWon != 0 || player.nbLost != 0){
+    server.send(200, "text/html", getHTML() + getStatHtml());
+  }
+  else{
+    server.send(200, "text/html", getHTML());  
+  }
   addPlayer();
 }
 
