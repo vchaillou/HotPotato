0b11431342cea4655e278743516f97ee8dd2ad37
vchaillou <valentin.chaillou@laposte.net>
2017-03-17 09:33:27 +0100

    Cleaning and comments

--- main/main.ino
+++ main/main.ino
@@ -3,6 +3,10 @@
 #include <ESP8266mDNS.h>
 #include <stdio.h>
 
+/*********************************/
+/***** CONSTANTS AND GLOBALS *****/
+/*********************************/
+
 #define MESH_PREFIX     "HOTPOTATO"
 #define MESH_PASSWORD   "Zodiaque"
 #define MESH_PORT       21147
@@ -13,17 +17,16 @@
 
 #define MAX_PLAYERS 10
 
-
 const int buttonPin = D2; // Button to thow the potato to another player
-const int yellowButtonPin = D5; // Potato owner
-const int redButtonPin = D7;    // Death Indicator
+const int redLightPin = D7;    // Death Indicator
 const int buzzerPin = D8;  // Buzzer "explosion"
 
 easyMesh mesh;
 
+// Structure that contains player data
 typedef struct {
   String name;
-  uint32_t node;
+  uint32_t node;    // needed for the sendSingle method
   int num;
   int nbWon;
   int nbLost;
@@ -35,10 +38,11 @@ int playerCount = 0;
 bool hasPotato = false;
 
 bool gameStarted = false;
-int timer = -1;
+int timer = -1;             // in seconds
 
 ESP8266WebServer server(WEBSERVER_PORT);
-const char * ssid = "Server_Potato";
+
+// HTML/CSS/JS Stuff
 const char * ContentHtml = "<!DOCTYPE html>\
                         <html>\
                           <head>\
@@ -68,19 +72,27 @@ const char * contentCss = "<style>\
                                 border-width: 1px;\ 
                               }\    
                           </style>";
+
+/***************************/
+/***** SETUP FUNCTIONS *****/
+/***************************/
+
+// Initializes the mesh network
 void setupMesh() {
   mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT );
   mesh.setReceiveCallback( &receivedCallback );
   mesh.setNewConnectionCallback( &newConnectionCallback ); 
 }
 
+// Intializes the pin modes for the button, the LED and the buzzer
 void setupPin() {
     pinMode(buttonPin, INPUT_PULLUP);
-    pinMode(yellowButtonPin, OUTPUT);
-    pinMode(redButtonPin, OUTPUT);
+    pinMode(redLightPin, OUTPUT);
     pinMode(buzzerPin, OUTPUT);
 }
 
+// Connects to the network
+// Uses defined constants NETWORK_SSID and NETWORK_PASSWORD for connection
 void setupWifi() {
   WiFi.begin(NETWORK_SSID, NETWORK_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
@@ -91,6 +103,10 @@ void setupWifi() {
  Serial.println(WiFi.localIP());
 }
 
+// Initializes the web server with 3 routes :
+//  - / (index)
+//  - /BeginGameWithPotato
+//  - /BeginGameWithoutPotato
 void setupServer() {
   if(MDNS.begin("esp8266")) {
     Serial.println("MDNS responder started");
@@ -102,10 +118,8 @@ void setupServer() {
   Serial.println("HTTP server started");
 }
 
-void setupMDNS() {
-  
-}
-
+// Setup that handles all of the above setups
+// Also add players
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -114,7 +128,6 @@ void setup() {
   setupMesh();
   setupWifi();
   setupServer();
-  setupMDNS();
 
   // TO CHANGE
   addPlayer(1022050, "Valentin C.");
@@ -122,6 +135,13 @@ void setup() {
   // TODO => VM
 }
 
+/*********************/
+/***** FUNCTIONS *****/
+/*********************/
+
+// Adds a player the player list
+// Just needs the node and the name
+// Counter is automatically incremented
 void addPlayer(uint32_t node, String name) {
   playerList[playerCount].node = node;
   playerList[playerCount].name = name;
@@ -131,71 +151,15 @@ void addPlayer(uint32_t node, String name) {
   playerCount++;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  server.handleClient();
-  mesh.update();
+/*********************************/
+/***** MESH NETWORK HANDLERS *****/
+/*********************************/
 
-  if(timer == 0) {
-    if(hasPotato) {
-      digitalWrite(buzzerPin, HIGH);
-      delay(250);
-      digitalWrite(buzzerPin, LOW);
-      for(int i=0 ; i<playerCount; i++) {
-        if(playerList[i].node == mesh.getChipId()) {
-          delay(1000); // just to be sure
-          String str = String(playerList[i].node);
-          while(mesh.connectionCount() < playerCount-1) {
-            mesh.update();
-            delay(100);
-          }
-          mesh.sendBroadcast(str);
-
-          break;
-
-        }
-      }
-      for(int i=0 ; i<playerCount; i++) {
-        if(playerList[i].node == mesh.getChipId()) {
-          playerList[i].nbLost += 1;
-        }
-        else {
-          playerList[i].nbWon += 1;
-        }
-        gameStarted = false;
-      }
-      setupWifi();
-    }
-    
-    hasPotato = false;
-    digitalWrite(redButtonPin, LOW);
-    timer = -1;
-  }
-  else if(timer > 0) {
-    timer--;
-  }
-
-
-
-  if(gameStarted && timer>0 && hasPotato && digitalRead(buttonPin) == LOW) {
-    Serial.println("Bouton !");
-    for(int i=0 ; i<playerCount ; i++) {
-      if(playerList[i].node == mesh.getChipId()) {
-        String str = String("YOURETHEPOTATOOWNER");
-        while(mesh.connectionCount() < playerCount-1) {
-          mesh.update();
-          delay(100);
-        }
-        mesh.sendSingle(playerList[(i+1)%playerCount].node, str);
-        digitalWrite(redButtonPin, LOW);
-        hasPotato = false;
-        break;
-      }
-    }
-  }
-  delay(1000);
-}
-
+// Handler called when a message is received from a node of the mesh network
+// 3 cases :
+//  - the game is not started => it's the timer, so start the game
+//  - the game is started and still running (timer > 0) => we get the potato
+//  - the game is started but timer is off => we get statistics and stop the game
 void receivedCallback(uint32_t from, String &msg) {
   if(!gameStarted) {
     timer = msg.toInt();
@@ -205,7 +169,7 @@ void receivedCallback(uint32_t from, String &msg) {
   }
   else if(timer > 0){
     hasPotato = true;
-    digitalWrite(redButtonPin, HIGH);
+    digitalWrite(redLightPin, HIGH);
   }
   else {
     gameStarted = false;
@@ -222,15 +186,22 @@ void receivedCallback(uint32_t from, String &msg) {
   }
 }
 
+// Handler called when a new node has been found
 void newConnectionCallback(bool adopt) {
   Serial.println("New node found");
 }
 
+/*****************************************/
+/***** HTML AND WEB SERVER FUNCTIONS *****/
+/*****************************************/
+
+// Handler for "/" route
 void webRoot() {
   server.send(200, "text/html", getHTML());
 }
 
-
+// Returns the HTML component that contains the table with statistics
+// Called from the function getHtml()
 String getStatHtml(){
   String statHead = "<br/><button id='id_button_table'>Show/Hide</button>\
                  <div id='id_div_table'><table>\
@@ -259,6 +230,8 @@ String getStatHtml(){
   return stat;
 }
 
+// Returns the main HTML component with the two buttons and the statistics
+// Buttons implicitely call the beginGameWithPotato and BeginGameWithoutPotato functions
 String getHTML() {
   String tmpContentHtml = ContentHtml;
   String tmpContentJquery = contentJquery;
@@ -275,32 +248,112 @@ String getHTML() {
   tmpContentHtml.replace(replaceOcc2, replaceOcc2 + tmpFormHtml);
   String statHtml = getStatHtml();
   
-  
   return tmpContentHtml + statHtml;
 }
 
+// Handler for "/BeginGameWithPotato" route
+// Launches the game
+// The player begins with the potato
+// Disconnects from the wifi network
+// Connects to the other players with mesh network
+// Initializes the timer with a random number between 10 seconds and 120 seconds
 void beginGameWithPotato() {
   server.send(200, "text/plain", "Game will be launched shortly...");
   WiFi.disconnect();
-  WiFi.mode(WIFI_OFF);
   while(mesh.connectionCount() < playerCount-1) {
     mesh.update();
     delay(1000);
   }
   gameStarted = true;
   hasPotato = true;
-  digitalWrite(redButtonPin, HIGH);
+  digitalWrite(redLightPin, HIGH);
   timer = random(10, 120);
   String str = String(timer);
   mesh.sendBroadcast(str);
   Serial.println(timer);
 }
 
+// Handler for "/BeginGameWithoutPotato" route
+// Disconnect from the wifi network so we can connect to mesh network
 void beginGameWithoutPotato() {
   server.send(200, "text/plain", "Waiting for the potato...");
   WiFi.disconnect();
-  WiFi.mode(WIFI_OFF);
 }
 
+/*********************/
+/***** MAIN LOOP *****/
+/*********************/
 
+// If the timer is over, trigger the end of the game and transfer the scores if the player lost
+// Else handle the button press if the game is running
+// Waits 1 second each time
+void loop() {
+  // put your main code here, to run repeatedly:
+  server.handleClient();
+  mesh.update();
+
+  // If the game is finished
+  if(timer == 0) {
+    if(hasPotato) {
+      digitalWrite(buzzerPin, HIGH);
+      delay(250);
+      digitalWrite(buzzerPin, LOW);
+
+      // Only the loser knows who lost and who won the game
+      for(int i=0 ; i<playerCount; i++) {
+        if(playerList[i].node == mesh.getChipId()) {
+          delay(1000);        // just to be sure the other players finished their game too
+          String str = String(playerList[i].node);
+          while(mesh.connectionCount() < playerCount-1) {
+            mesh.update();
+            delay(100);
+          }
+          Serial.println("Sending Score...");
+          mesh.sendBroadcast(str);
+          break;
+        }
+      }
+      for(int i=0 ; i<playerCount; i++) {
+        if(playerList[i].node == mesh.getChipId()) {
+          playerList[i].nbLost += 1;
+        }
+        else {
+          playerList[i].nbWon += 1;
+        }
+        gameStarted = false;
+      }
+      setupWifi();
+    }
+    
+    hasPotato = false;
+    digitalWrite(redLightPin, LOW);
+    timer = -1;
+  }
+  else if(timer > 0) {
+    timer--;
+  }
+  // End If the game is finished
+
+  int buttonPressed = digitalRead(buttonPin);
+
+  // If button pressed
+  if(gameStarted && timer>0 && hasPotato && buttonPressed == LOW) {
+    Serial.println("Bouton !");
+    for(int i=0 ; i<playerCount ; i++) {
+      if(playerList[i].node == mesh.getChipId()) {
+        String str = String("YOURETHEPOTATOOWNER");
+        while(mesh.connectionCount() < playerCount-1) {
+          mesh.update();
+          delay(100);
+        }
+        mesh.sendSingle(playerList[(i+1)%playerCount].node, str);
+        digitalWrite(redLightPin, LOW);
+        hasPotato = false;
+        break;
+      }
+    }
+  }
+  // End If button pressed
+  delay(1000);
+}
 
